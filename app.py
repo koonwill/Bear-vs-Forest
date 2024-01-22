98cc238f66185f39f6394b0e87bfdb58c839e0d3
Siravich Termvadsayanon <93331101+koonwill@users.noreply.github.com>
2024-01-22 15:12:19 +0700

    done

---
+++ app.py
@@ -0,0 +1,19 @@
+import gradio as gr
+from fastai.vision.all import *
+
+
+def classify_image(img):
+    learn = load_learner('model.pkl')
+    img = PILImage.create(img)
+    img.thumbnail((128, 128))
+    pred, idx, probs = learn.predict(img)
+
+    if pred == 'bear':
+        result = f"The image is a Bear with probability {probs[idx]:.04f}"
+    if pred == 'forest':
+        result = f"The image is a Forest with probability {probs[idx]:.04f}"
+    return result
+
+example = ["bear.jpg", "forest.jpg"]
+iface = gr.Interface(classify_image, gr.inputs.Image(shape=(128, 128)), "text", examples=example)
+iface.launch(share=True)
